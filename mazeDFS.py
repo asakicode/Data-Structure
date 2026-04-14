f9e59de28fca8baf50ca6dc33e0794c34e9f546e
주형짱 <101456100+asakicode@users.noreply.github.com>
2026-04-14 14:29:26 +0900

    Implement DFS algorithm for maze traversal

---
+++ mazeDFS.py
@@ -0,0 +1,49 @@
+from arrayStack import ArrayStack
+
+
+map = [
+    ['1','1','1','1','1','1'],
+    ['e','0','0','0','0','1'],
+    ['1','0','1','0','1','1'],
+    ['1','1','1','0','0','x'],
+    ['1','1','1','0','1','1'],
+    ['1','1','1','1','1','1'],
+
+]
+
+SIZE = 6
+
+def isValidPos(r,c):
+    if 0 <= r < SIZE and 0 <= c < SIZE:
+        if map[r][c] == '0' or map[r][c] == 'x':
+            return True
+    return False
+
+def DFS():
+    print('DFS: ')
+    S = ArrayStack()
+    S.push((1,0))
+
+    while not S.isEmpty():
+        pos = S.pop()
+        print(pos, end = ' -> ')
+        (r, c) = pos
+
+        if (map[r][c] == 'x'):  # 출구이면 True 를 반환
+            return True
+
+        else:               # 출구가 아니면 .을 찍어버린다
+            map[r][c] = '.'
+
+            if isValidPos(r-1, c): S.push((r-1, c))
+            if isValidPos(r+1, c): S.push((r+1, c))
+            if isValidPos(r, c-1): S.push((r, c-1))
+            if isValidPos(r, c+1): S.push((r, c+1))
+        print(S)
+    return False
+
+result = DFS()
+if result:
+    print("succs")
+else:
+    print("fails")
